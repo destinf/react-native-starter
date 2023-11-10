77d9622b1934642aff222de82db5c38aeccfeb59
d <d@example.com>
2023-11-10 00:17:24 -0500

    Upgrade storybook

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,38 +1,25 @@
+import { dirname, join } from "path";
 const path = require('path')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 const config = {
   stories: ['../../../packages/ui/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    'storybook-addon-next-router',
-    // "storybook-addon-next",
-    {
-      name: '@storybook/addon-react-native-web',
-      options: {
-        modulesToTranspile: [
-          'solito',
-          'react-native-web',
-          'expo-linking',
-          'expo-constants',
-          'expo-modules-core',
-          'react-i18next',
-          'expo-document-picker',
-          'expo-av',
-          'expo-asset',
-          // '@my/config',
-        ],
-        babelPlugins: [
-          // "react-native-reanimated/plugin", // this breaks...
-        ],
-      },
-    },
-  ],
-  core: {
-    builder: 'webpack5',
+
+  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), {
+    name: '@storybook/addon-react-native-web',
+    options: {
+      modulesToTranspile: ['solito', 'react-native-web', 'expo-linking', 'expo-constants', 'expo-modules-core', 'react-i18next', 'expo-document-picker', 'expo-av', 'expo-asset'],
+      babelPlugins: []
+    }
+  }],
+
+  core: {},
+
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {}
   },
-  framework: '@storybook/react',
+
   typescript: {
     check: false,
     checkOptions: {},
@@ -50,6 +37,7 @@ const config = {
       propFilter: (prop: any) => (prop.parent ? !/node_modules\/(?!tamagui)/.test(prop.parent.fileName) : true),
     },
   },
+
   // managerWebpack: (config, options) => {
   //     options.cache.set = () => Promise.resolve();
   //     return config;
@@ -80,9 +68,18 @@ const config = {
     // )
     return config
   },
+
   env: (config: any) => ({
     ...config,
     TAMAGUI_TARGET: 'web',
   }),
+
+  docs: {
+    autodocs: true
+  }
 }
 export default config
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
